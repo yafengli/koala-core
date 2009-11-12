ba1d681bd02ed2c6a0486241f10ddcda5e0b2c42
yafengli.dream <yafengli.dream@409fc1a2-acc0-11de-97f0-97815e7b2756>
2009-11-12 07:42:54 +0000

    milestone:2009-11-12-1

---
+++ trunk/demo-apps/hpt/src/main/java/cn/hpt/util/HelperUtil.java
@@ -0,0 +1,26 @@
+package cn.hpt.util;
+
+import java.util.Random;
+import java.util.regex.Pattern;
+
+/**
+ *
+ * @author Administrator
+ */
+public class HelperUtil {
+
+    public static synchronized String createRandomString(int size) {// 随机字符串
+        char[] c = {'1', '2', '3', '4', '5', '6', '7', '8', '9', '0'};
+        Random random = new Random(); // 初始化随机数产生器
+        StringBuffer sb = new StringBuffer();
+        for (int i = 0; i < size; i++) {
+            sb.append(c[Math.abs(random.nextInt(10)) % c.length]);
+        }
+        return sb.toString();
+    }
+
+    public static synchronized boolean isMatch(String str, String regEx) {
+        Pattern ep = Pattern.compile(regEx);
+        return ep.matcher(str).matches();
+    }
+}
