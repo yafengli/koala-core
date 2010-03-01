7f389f37e4740372a8bf1ac2e6379f0fca493784
yafengli.dream <yafengli.dream@409fc1a2-acc0-11de-97f0-97815e7b2756>
2010-03-01 05:04:58 +0000

    add

--- trunk/extend-jpa-ejb/src/test/java/org/helloworld/QueueTest.java
+++ trunk/extend-jpa-ejb/src/test/java/org/helloworld/QueueTest.java
@@ -1,79 +1,40 @@
 package org.helloworld;
 
-import hellojms.queue.TextListener;
+import javax.jms.JMSException;
+import javax.jms.Message;
+import javax.jms.Session;
+import javax.jms.TextMessage;
+
 import org.junit.Before;
 import org.junit.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
-
-import javax.jms.*;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import org.springframework.jms.core.JmsTemplate;
+import org.springframework.jms.core.MessageCreator;
 
 /**
- * Created by IntelliJ IDEA.
- * User: Administrator
- * Date: 2010-1-12
- * Time: 10:34:23
+ * Created by IntelliJ IDEA. User: Administrator Date: 2010-1-12 Time: 10:34:23
  * To change this template use File | Settings | File Templates.
  */
 public class QueueTest {
-    private ApplicationContext ctx;
+	private ApplicationContext ctx;
+	private JmsTemplate jmsTemplate;
 
-    @Before
-    public void init() {
-        ctx = new ClassPathXmlApplicationContext(new String[]{"META-INF/applicationContext.xml"});
-    }
+	@Before
+	public void init() {
+		ctx = new ClassPathXmlApplicationContext(
+				new String[] { "META-INF/applicationContext.xml" });
+		jmsTemplate = (JmsTemplate) ctx.getBean("jmsTemplate");
+	}
 
-    @Test
-    public void testPool() {
-        // waiting...
-        waiting();
-    }
-
-//    @Test
-
-    public void testLook() {
-        Queue q = (Queue) ctx.getBean("queue");
-        QueueConnectionFactory qcf = (QueueConnectionFactory) ctx.getBean("connectionFactory");
-        QueueConnection qc = null;
-        QueueSession qs = null;
-        QueueReceiver qr = null;
-        try {
-            qc = qcf.createQueueConnection();
-            qs = qc.createQueueSession(false, Session.AUTO_ACKNOWLEDGE);
-            qr = qs.createReceiver(q);
-            qr.setMessageListener(new TextListener());
-            qc.start();
-            // waiting...
-            waiting();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        finally {
-            try {
-                qs.close();
-                qr.close();
-                qc.stop();
-            }
-            catch (Exception e) {
-                e.printStackTrace();
-            }
-        }
-    }
-
-    private void waiting() {
-        /*quit*/
-        System.out.println("Enter 'q' and press <return> to exit ");
-        InputStreamReader isr = new InputStreamReader(System.in);
-        char response = '\0';
-        while (!((response == 'q') || (response == 'Q'))) {
-            try {
-                response = (char) isr.read();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
-        System.out.println("End listening!");
-    }
+	@Test
+	public void testLook() {
+		jmsTemplate.send(new MessageCreator() {
+			public Message createMessage(Session session) throws JMSException {
+				TextMessage tm = session.createTextMessage();
+				tm.setText("Fuck !");
+				return tm;
+			}
+		});
+	}
 }
