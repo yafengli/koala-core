d5b6057503bb409c0982ffed95d80073094a5a49
yafengli.dream <yafengli.dream@409fc1a2-acc0-11de-97f0-97815e7b2756>
2010-03-22 07:33:09 +0000

    add feature:pretty faces jsf url rewrite.

--- trunk/koala-jsf/src/main/java/cn/demo/HelloWorld.java
+++ trunk/koala-jsf/src/main/java/cn/demo/HelloWorld.java
@@ -14,6 +14,8 @@ public class HelloWorld {
 	@ManagedProperty(name = "name", value = "@FUCK@")
 	private String name;
 
+	private Long id;
+
 	public String getName() {
 		return name;
 	}
@@ -22,8 +24,17 @@ public class HelloWorld {
 		this.name = name;
 	}
 
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
 	public String action() {
-		System.out.printf("[%s][%s]\n","Hello World!",this.getName());
+		System.out.printf("[%s][%s,%s]\n", "Hello World!", this.getName(), this
+				.getId());
 		HttpServletRequest req = (HttpServletRequest) FacesContext
 				.getCurrentInstance().getExternalContext().getRequest();
 
