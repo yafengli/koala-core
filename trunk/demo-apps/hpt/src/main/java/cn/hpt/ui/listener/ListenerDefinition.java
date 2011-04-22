2e085b8f0c7ba86c7befddc7f35a030b4fd57232
yafengli.dream <yafengli.dream@409fc1a2-acc0-11de-97f0-97815e7b2756>
2011-04-22 05:39:37 +0000

    clear

--- trunk/demo-apps/hpt/src/main/java/cn/hpt/ui/listener/ListenerDefinition.java
+++
@@ -1,9 +0,0 @@
-package cn.hpt.ui.listener;
-
-public class ListenerDefinition {
-
-	public static final String CLOSE_BEAN_NAME = "closeAppActionListener";
-	public static final String LOGIN_BEAN_NAME = "loginActionListener";
-	public static final String CHANGE_LOGIN_BEAN_NAME = "changeLoginActionListener";
-
-}
