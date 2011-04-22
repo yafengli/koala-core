eab4f3c60253d91cd57e4d3fde3935e678e286c4
yafengli.dream <yafengli.dream@409fc1a2-acc0-11de-97f0-97815e7b2756>
2011-04-22 01:58:05 +0000

    2.1-m4

--- tags/1.0/koala-core/src/test/java/test/dao/AccountIBatisDao.java
+++
@@ -1,9 +0,0 @@
-package test.dao;
-
-
-import org.koala.dao.IGenericDao;
-import org.koala.dao.IGenericIBatisDao;
-import test.model.Account;
-
-public interface AccountIBatisDao extends IGenericIBatisDao<Account,Long>{
-}
--- tags/1.0/koala-core/src/test/java/test/dao/ExUserDao.java
+++
@@ -1,8 +0,0 @@
-package test.dao;
-
-import org.koala.dao.IGenericDao;
-import test.model.ExUser;
-
-public interface ExUserDao extends IGenericDao<ExUser, Integer> {
-	public int service(String username);
-}
