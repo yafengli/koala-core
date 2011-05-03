112c3d20e684416522284a0932bb41f8a40636b4
yafengli.dream <yafengli.dream@409fc1a2-acc0-11de-97f0-97815e7b2756>
2011-05-03 06:16:37 +0000

    2.1-m12

--- trunk/koala-core/src/test/java/test/test/SecurityTest.java
+++ trunk/koala-core/src/test/java/test/test/SecurityTest.java
@@ -3,8 +3,6 @@ package test.test;
 import org.junit.Test;
 import org.koala.util.MD5Tool;
 import org.koala.util.SecurityTool;
-import sun.misc.BASE64Decoder;
-import sun.misc.BASE64Encoder;
 
 import javax.crypto.Cipher;
 import javax.crypto.KeyGenerator;
@@ -17,120 +15,117 @@ import java.security.KeyPairGenerator;
 import java.security.PrivateKey;
 import java.security.PublicKey;
 import java.util.Set;
+import org.koala.util.BASE64;
 
 public class SecurityTest {
-	public static final String plainText = "中环人民Hello World,arekidding me!!~~";
 
-	//	@Test
-	public void testFind() {
-		Set<String> set = SecurityTool.findServiceTypes();
-		for (String serviceType : set) {
-			System.out.println("@" + serviceType);
-			Set<String> csets = SecurityTool.findCryptoImpls(serviceType);
-			for (String crypto : csets) {
-				System.out.println("#" + crypto);
-			}
-		}
-		set = SecurityTool.findCryptoImpls("Cipher");
-		for (String name : set) {
-			System.out.println("$" + name);
-		}
-		set = SecurityTool.findCryptoImpls("KeyPairGenerator");
-		for (String name : set) {
-			System.out.println("#" + name);
-		}
-	}
+    public static final String plainText = "中环人民Hello World,arekidding me!!~~";
 
-	//	@Test
-	public void testMac() {
+    //	@Test
+    public void testFind() {
+        Set<String> set = SecurityTool.findServiceTypes();
+        for (String serviceType : set) {
+            System.out.println("@" + serviceType);
+            Set<String> csets = SecurityTool.findCryptoImpls(serviceType);
+            for (String crypto : csets) {
+                System.out.println("#" + crypto);
+            }
+        }
+        set = SecurityTool.findCryptoImpls("Cipher");
+        for (String name : set) {
+            System.out.println("$" + name);
+        }
+        set = SecurityTool.findCryptoImpls("KeyPairGenerator");
+        for (String name : set) {
+            System.out.println("#" + name);
+        }
+    }
 
-		try {
-			SecretKey key = getKey();
-			if (key != null) {
-				System.out.println("密钥：" + MD5Tool.getInstance().byteArrayToHexString(key.getEncoded()) + ":" + key.getAlgorithm());
-				Mac mac = Mac.getInstance("HmacMD5");
-				mac.init(key);
-				mac.update(plainText.getBytes());
-				System.out.println("MAC:" + MD5Tool.getInstance().byteArrayToHexString(mac.doFinal()));
-			}
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
+    //	@Test
+    public void testMac() {
 
-	//	@Test
-	public void testFc() {
-		try {
-			BASE64Decoder bd = new BASE64Decoder();
-			BASE64Encoder be = new BASE64Encoder();
-			KeyPairGenerator kpg = KeyPairGenerator.getInstance("RSA");
-			kpg.initialize(512);
-			KeyPair kp = kpg.genKeyPair();
-			PrivateKey privateKey = kp.getPrivate();
-			PublicKey publicKey = kp.getPublic();
+        try {
+            SecretKey key = getKey();
+            if (key != null) {
+                System.out.println("密钥：" + MD5Tool.getInstance().byteArrayToHexString(key.getEncoded()) + ":" + key.getAlgorithm());
+                Mac mac = Mac.getInstance("HmacMD5");
+                mac.init(key);
+                mac.update(plainText.getBytes());
+                System.out.println("MAC:" + MD5Tool.getInstance().byteArrayToHexString(mac.doFinal()));
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
-			Cipher cp = Cipher.getInstance("RSA");
-			cp.init(Cipher.ENCRYPT_MODE, publicKey);
+    //	@Test
+    public void testFc() {
+        try {
+            KeyPairGenerator kpg = KeyPairGenerator.getInstance("RSA");
+            kpg.initialize(512);
+            KeyPair kp = kpg.genKeyPair();
+            PrivateKey privateKey = kp.getPrivate();
+            PublicKey publicKey = kp.getPublic();
 
-			String str = be.encode(cp.doFinal(plainText.getBytes()));
-			System.out.println(str);
+            Cipher cp = Cipher.getInstance("RSA");
+            cp.init(Cipher.ENCRYPT_MODE, publicKey);
 
-			cp.init(Cipher.DECRYPT_MODE, privateKey);
-			System.out.println(new String(cp.doFinal(bd.decodeBuffer(str))));
+            String str = new String(BASE64.encode(cp.doFinal(plainText.getBytes())));
+            System.out.println(str);
 
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
+            cp.init(Cipher.DECRYPT_MODE, privateKey);
+            System.out.println(new String(cp.doFinal(BASE64.decode(str))));
 
-	@Test
-	public void testKey() {
-		try {
-			BASE64Encoder be = new BASE64Encoder();
-			BASE64Decoder bd = new BASE64Decoder();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void testKey() {
+        try {
 //			KeyGenerator kg = KeyGenerator.getInstance("DES");
 //			SecretKey key = kg.generateKey();
 //			String keyStr = be.encode(key.getEncode());
-			String keyStr = "LIMaBAEE99w=";
-			SecretKey key = new SecretKeySpec(bd.decodeBuffer(keyStr), "DES");
+            String keyStr = "LIMaBAEE99w=";
+            SecretKey key = new SecretKeySpec(BASE64.decode(keyStr), "DES");
 
-			Cipher cp = Cipher.getInstance("DES");
-			cp.init(Cipher.ENCRYPT_MODE, key);
-			int blockSize = cp.getBlockSize();
+            Cipher cp = Cipher.getInstance("DES");
+            cp.init(Cipher.ENCRYPT_MODE, key);
+            int blockSize = cp.getBlockSize();
 
-			ByteBuffer inbuffer = ByteBuffer.wrap(plainText.getBytes());
-			inbuffer.flip();
-			int outSize=cp.getOutputSize(inbuffer.capacity());
-			System.out.println("##"+outSize);
-			ByteBuffer outbuffer=ByteBuffer.allocate(outSize);
-			cp.update(inbuffer,outbuffer);
+            ByteBuffer inbuffer = ByteBuffer.wrap(plainText.getBytes());
+            inbuffer.flip();
+            int outSize = cp.getOutputSize(inbuffer.capacity());
+            System.out.println("##" + outSize);
+            ByteBuffer outbuffer = ByteBuffer.allocate(outSize);
+            cp.update(inbuffer, outbuffer);
 
-			String str1 = be.encode(cp.doFinal());
-			System.out.println("@1=@" + str1);
+            String str1 = new String(BASE64.encode(cp.doFinal()));
+            System.out.println("@1=@" + str1);
 
-			String str = be.encode(cp.doFinal(plainText.getBytes()));
-			System.out.println(str);
+            String str = new String(BASE64.encode(cp.doFinal(plainText.getBytes())));
+            System.out.println(str);
 
-			cp.init(Cipher.DECRYPT_MODE, key);
-			byte[] dbs = cp.doFinal(bd.decodeBuffer(str));
-			System.out.println(new String(dbs));
+            cp.init(Cipher.DECRYPT_MODE, key);
+            byte[] dbs = cp.doFinal(BASE64.decode(str));
+            System.out.println(new String(dbs));
 
-			System.out.println("@1=@" + new String(cp.doFinal(bd.decodeBuffer(str1))));
+            System.out.println("@1=@" + new String(cp.doFinal(BASE64.decode(str1))));
 
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
-
-	public SecretKey getKey() {
-		SecretKey key = null;
-		try {
-			KeyGenerator kg = KeyGenerator.getInstance("DES");
-			key = kg.generateKey();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		return key;
-	}
+    public SecretKey getKey() {
+        SecretKey key = null;
+        try {
+            KeyGenerator kg = KeyGenerator.getInstance("DES");
+            key = kg.generateKey();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return key;
+    }
 }
