2b5a37577a6cf99475a66292cd20ee5aec3af59a
yafengli.dream <yafengli.dream@409fc1a2-acc0-11de-97f0-97815e7b2756>
2011-05-03 02:29:18 +0000

    2.1-m10

---
+++ trunk/koala-core/src/test/java/test/test/GenTest.java
@@ -0,0 +1,29 @@
+package test.test;
+
+import java.util.List;
+import java.util.ArrayList;
+import java.io.Serializable;
+
+/**
+ * Date: 2009-9-24
+ * Time: 14:09:26
+ *
+ * @version 1.0
+ * @authtor YaFengLi
+ */
+public class GenTest {
+
+    public <T extends Object, K extends Serializable> List<T> make(T first, K k) {
+        System.out.printf("[%s]\n[%s]\n", first.getClass().getName(), k.getClass().getName());
+        return new ArrayList<T>();
+    }
+
+    @Test
+    public void testGen() {
+        GenTest tg = new GenTest();
+        tg.make("One", 1);
+        tg.make(1, "One");
+        tg.make(1.0, true);
+        tg.make(true, "232");
+    }
+}
